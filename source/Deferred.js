e2db2b002ade018d7fffb93068454060ecbdd3cc
zhanghexuan <136789658@qq.com>
2016-07-25 12:14:50 +0800

    jQuery.Deferred()模块源码

---
+++ source/Deferred.js
@@ -0,0 +1,97 @@
+jQuery.extend({
+	/**
+	 * 在CallBacks(flags)的基础上，为回调函数增加了状态，提供多个回调函数列表
+	 */
+	Deferred: function(func) {
+		var doneList = jQuery.Callbacks("once memory"),
+			failList = jQuery.Callbacks("once memory"),
+			progressList = jQuery.Callbacks("memory"),
+			state = "pending",
+			lists = {
+				resolve: doneList,
+				reject: failList,
+				notify: progressList
+			},
+			promise = {
+				done: doneList.add,
+				fail: failList.add,
+				progress: progressList.add,
+
+				state: function() {
+					return state;
+				},
+
+				// Deprecated
+				isResolved: doneList.fired,
+				isRejected: failList.fired,
+
+				then: function(doneCallbacks, failCallbacks, progressCallbacks) {
+					deferred.done(doneCallbacks).fail(failCallbacks).progress(progressCallbacks);
+					return this;
+				},
+				always: function() {
+					deferred.done.apply(deferred, arguments).fail.apply(deferred, arguments);
+					return this;
+				},
+				pipe: function(fnDone, fnFail, fnProgress) {
+					return jQuery.Deferred(function(newDefer) {
+						jQuery.each({
+							done: [fnDone, "resolve"],
+							fail: [fnFail, "reject"],
+							progress: [fnProgress, "notify"]
+						}, function(handler, data) {
+							var fn = data[0],
+								action = data[1],
+								returned;
+							if (jQuery.isFunction(fn)) {
+								deferred[handler](function() {
+									returned = fn.apply(this, arguments);
+									if (returned && jQuery.isFunction(returned.promise)) {
+										returned.promise().then(newDefer.resolve, newDefer.reject, newDefer.notify);
+									} else {
+										newDefer[action + "With"](this === deferred ? newDefer : this, [returned]);
+									}
+								});
+							} else {
+								deferred[handler](newDefer[action]);
+							}
+						});
+					}).promise();
+				},
+				// Get a promise for this deferred
+				// If obj is provided, the promise aspect is added to the object
+				promise: function(obj) {
+					if (obj == null) {
+						obj = promise;
+					} else {
+						for (var key in promise) {
+							obj[key] = promise[key];
+						}
+					}
+					return obj;
+				}
+			},
+			deferred = promise.promise({}),
+			key;
+
+		for (key in lists) {
+			deferred[key] = lists[key].fire;
+			deferred[key + "With"] = lists[key].fireWith;
+		}
+
+		// Handle state
+		deferred.done(function() {
+			state = "resolved";
+		}, failList.disable, progressList.lock).fail(function() {
+			state = "rejected";
+		}, doneList.disable, progressList.lock);
+
+		// Call given func if any
+		if (func) {
+			func.call(deferred, deferred);
+		}
+
+		// All done!
+		return deferred;
+	}
+});
